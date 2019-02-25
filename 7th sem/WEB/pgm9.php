16a72d9059d866870f6a5c616a6b493e1dd31945
Rutuja Rajesh <rutujarajesh23@gmail.com>
2019-02-25 15:13:14 +0530

    Create pgm9.php

---
+++ 7th sem/WEB/pgm9.php
@@ -0,0 +1,38 @@
+<?php
+header('Content-Type: text/plain');
+$allTheStates = "Mississippi Alabama Texas Massachusetts Kansas";
+$statesArray = [];
+$states1 = explode(' ', $allTheStates);
+$i = 0;
+foreach ($states1 as $state) {
+if (preg_match('/xas$/', ($state))) {
+$statesArray[$i] = ($state);
+$i = $i + 1;
+print "\nThe States that ends in xas:" . $state;
+}
+}
+foreach ($states1 as $state) {
+if (preg_match('/^k.*s$/i', ($state))) {
+$statesArray[$i] = ($state);
+$i = $i + 1;
+echo "\nThe states that begins with k ans ends in s:" . $state;
+}
+}
+foreach($states1 as $state) {
+if (preg_match('/^M.*s$/', ($state))) {
+$statesArray[$i] = ($state);
+$i = $i + 1;
+echo "\nThe states that begins with M and ends in s:" . $state;
+}
+}
+foreach($states1 as $state) {
+if (preg_match('/a$/', ($state))) {
+$statesArray[$i] = ($state);
+$i = $i + 1;
+echo "\nThe states that ends in a:" . $state;
+}
+}
+foreach ($statesArray as $element => $value) {
+print( "\n" . $value . " is the element " . $element);
+}
+?>
